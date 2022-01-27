0e762f75c328220288a4b091e642b294f2a09593
jcpcabanada <jcpcabanada@gmail.com>
2022-01-26 18:37:43 -0800

    3rd

--- index.test.js
+++ index.test.js
@@ -16,29 +16,42 @@ describe('[Exercise 1] trimProperties', () => {
 })
 
 describe('[Exercise 2] trimPropertiesMutation', () => {
-  // test('[3] returns an object with the properties trimmed', () => {})
-  // test('[4] the object returned is the exact same one we passed in', () => {})
+  test('[3] returns an object with the properties trimmed', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const expected = { foo: 'foo', bar: 'bar', baz: 'baz' }
+    const actual = utils.trimProperties(input)
+    expect(actual).toEqual(expected)
+  })
+  test('[4] the object returned is the exact same one we passed in', () => {
+    const input = { foo: '  foo ', bar: 'bar ', baz: ' baz' }
+    const actual = utils.trimPropertiesMutation(input)
+    expect(actual).toBe(input)
+  })
 })
 
 describe('[Exercise 3] findLargestInteger', () => {
-  // test('[5] returns the largest number in an array of objects { integer: 2 }', () => {})
+  test('[5] returns the largest number in an array of objects { integer: 2 }', () => {
+    const input = [{ integer: 1 }, { integer: 3 }, { integer: 2 }]
+    const actual = utils.findLargestInteger(input)
+    expect(actual).toBe(3)
+  })
 })
 
 describe('[Exercise 4] Counter', () => {
-  let counter
-  beforeEach(() => {
-    counter = new utils.Counter(3) // each test must start with a fresh couter
-  })
+  // let counter
+  // beforeEach(() => {
+  //   counter = new utils.Counter(3) // each test must start with a fresh couter
+  // })
   // test('[6] the FIRST CALL of counter.countDown returns the initial count', () => {})
   // test('[7] the SECOND CALL of counter.countDown returns the initial count minus one', () => {})
   // test('[8] the count eventually reaches zero but does not go below zero', () => {})
 })
 
 describe('[Exercise 5] Seasons', () => {
-  let seasons
-  beforeEach(() => {
-    seasons = new utils.Seasons() // each test must start with fresh seasons
-  })
+  // let seasons
+  // beforeEach(() => {
+  //   seasons = new utils.Seasons() // each test must start with fresh seasons
+  // })
   // test('[9] the FIRST call of seasons.next returns "summer"', () => {})
   // test('[10] the SECOND call of seasons.next returns "fall"', () => {})
   // test('[11] the THIRD call of seasons.next returns "winter"', () => {})
@@ -48,10 +61,10 @@ describe('[Exercise 5] Seasons', () => {
 })
 
 describe('[Exercise 6] Car', () => {
-  let focus
-  beforeEach(() => {
-    focus = new utils.Car('focus', 20, 30) // each test must start with a fresh car
-  })
+  // let focus
+  // beforeEach(() => {
+  //   focus = new utils.Car('focus', 20, 30) // each test must start with a fresh car
+  // })
   // test('[15] driving the car returns the updated odometer', () => {})
   // test('[16] driving the car uses gas', () => {})
   // test('[17] refueling allows to keep driving', () => {})
